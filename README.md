95369851eda38461af5fe4c39ec3d40c7d97c860
Pirkhs <nathan.briers32@gmail.com>
2024-03-22 12:01:13 +0000

    initial native and firebase setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# Plan-Perfect-2
---
+++ firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { Key } from './ApiKey'
+
+const firebaseConfig = {
+  apiKey: Key,
+  authDomain: "test-database-plan-perfect.firebaseapp.com",
+  databaseURL:
+    "https://test-database-plan-perfect-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "test-database-plan-perfect",
+  storageBucket: "test-database-plan-perfect.appspot.com",
+  messagingSenderId: "65580804131",
+  appId: "1:65580804131:web:ba66f055806e6c0940e996",
+  measurementId: "G-SPRR2Z1T24",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+//Initialise Auth
+export const auth = getAuth(app);
+
+//database fetch
+export const db = getFireStore();
