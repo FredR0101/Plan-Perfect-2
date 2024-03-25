84fbb7c27ae96d63b537b1e082a476341a19ed36
sclotaire <s.clotaire@hotmail.co.uk>
2024-03-25 16:33:15 +0200

    completed homepage, on add activity

--- Components/HomePage.js
+++ Components/HomePage.js
@@ -1,7 +1,59 @@
-import { Text } from "react-native"
+import { Pressable, Text , Button} from "react-native"
+import { db } from "../firebase"
+import {doc, getDocs, collection, query, where} from 'firebase/firestore'
+import { useEffect, useState } from "react"
+import { View } from "react-native-web"
+import { AddItinerary } from "./AddItinerary"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export const HomePage = () => {
+
+export const HomePage = ({navigation}) => {
+    const [itinerary, setItinerary] = useState([])
+
+    useEffect(() => {
+        const fetchData = collection(db, "test-itineraries")
+        getDocs(fetchData).then((data) => {
+            const itineraryData = []
+            data.docs.forEach((doc) => {
+                itineraryData.push({...doc.data(), id:doc.id})
+            })
+            setItinerary(itineraryData)
+        }) 
+        
+       
+
+        // if (docSnap.exists()) {
+        //     console.log("Document data:", docSnap.data());
+        //   } else {
+        //     // docSnap.data() will be undefined in this case
+        //     console.log("No such document!");
+        //   }
+
+    }, [])
+      
+
+    const Tab = createBottomTabNavigator();
     return (
-        <Text>Hello!</Text>
+        <>
+            <Text>Your Itineraries</Text>
+
+            <View>
+                <Text>{itinerary.map((data) => {
+                    console.log(data);
+                    return (
+                    <View key={data.id}>
+                        <Text>Name: {data.name} </Text>
+                        <Text>Location: {data.location}</Text>
+                        <Text>Dates: {data.startDate} to {data.endDate}</Text>
+                        </View>
+                    )
+                })}</Text>
+            </View>
+            
+            <Tab.Navigator screenOptions={{headerShown: false}} initialRouteName='HomePage'>
+            <Tab.Screen name="Add Itinerary" component={AddItinerary}/>
+            </Tab.Navigator>
+
+        </>
     )
 }
--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,6 +1,6 @@
 import * as React from "react"; 
 import {createDrawerNavigator} from '@react-navigation/drawer'
-import { Home } from "./Home";
+import { HomePage } from "./HomePage.js";
 import {Recommendations} from './Recommendations'
 import {MainProfile} from "./MainProfile"
 
@@ -11,7 +11,7 @@ export const Navigation = () => {
   return (
     <>
       <Drawer.Navigator>
-          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name="Home" component={HomePage} />
           <Drawer.Screen name="Recommendations" component={Recommendations} />
           <Drawer.Screen name="Profile" component={MainProfile} />
       </Drawer.Navigator>
