6d5ff68369cb01292b5fa16f734d23019c4ebd99
FredR0101 <fredreed77@gmail.com>
2024-03-26 16:18:37 +0000

    Sorted database queries

--- Components/EditUser.js
+++ Components/EditUser.js
@@ -1,53 +1,121 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import { TextInput } from "react-native-web";
+import { KeyboardAvoidingView, TextInput } from "react-native-web";
 import React, { useState } from "react";
 
 import { auth } from "../firebase";
-import{ db } from "../firebase"
-import {updateDoc, doc} from 'firebase/firestore'
+import { db } from "../firebase";
+import { updateDoc, doc } from "firebase/firestore";
 
 const EditUser = () => {
   const [image, setImage] = useState("");
   const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const handleSubmit = () => {
-    const uid = auth.currentUser.uid
-    const ref = doc(db, "test-users", uid)
-   
-   updateDoc(ref, { username: username } ).then(() => {
-      alert("Username successfully changed")
-   }).catch((err)=> {
-      alert(err,"Something went wrong")
-    })
-}
+
+  const handleUsernameSubmit = () => {
+    const uid = auth.currentUser.uid;
+    const ref = doc(db, "test-users", uid);
+
+    updateDoc(ref, { username: username })
+      .then(() => {
+        alert("Username successfully updated");
+        setImage("");
+        setUsername("");
+      })
+      .catch((err) => {
+        alert(err, "Something went wrong");
+      });
+  };
+
+  const handleImageSubmit = () => {
+    const uid = auth.currentUser.uid;
+    const ref = doc(db, "test-users", uid);
+
+    updateDoc(ref, { image: image })
+      .then(() => {
+        alert("Profile picture successfully updated");
+        setImage("");
+        setUsername("");
+      })
+      .catch((err) => {
+        alert(err, "Something went wrong");
+      });
+  };
 
   return (
-    <View>
-      <TextInput
-        value={image}
-        placeholder="Image url"
-        onChangeText={(text) => setImage(text)}
-        style={styles.input}
-      />
-      <TextInput
-        value={username}
-        placeholder="Username"
-        onChangeText={(text) => setUsername(text)}
-        style={styles.input}
-      />
-      <TextInput
-        value={email}
-        placeholder="Email"
-        onChangeText={(text) => setEmail(text)}
-        style={styles.input}
-      />
-    <Pressable onPress={handleSubmit}>
-      <Text>Update</Text>
-    </Pressable>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={username}
+          placeholder="Username"
+          onChangeText={(text) => setUsername(text)}
+          style={styles.input}
+        />
+        <Pressable onPress={handleUsernameSubmit} style={styles.btn}>
+          <Text style={styles.btntext}>Update Username</Text>
+        </Pressable>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={image}
+          placeholder="Image url"
+          onChangeText={(text) => setImage(text)}
+          style={styles.input}
+        />
+        <Pressable onPress={handleImageSubmit} style={styles.btn}>
+          <Text style={styles.btntext}>Update Image</Text>
+        </Pressable>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default EditUser;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    },
+    inputContainer: {
+    width: "80%",
+    marginBottom: 20,
+    },
+    input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+    marginBottom: 10,
+    },
+    btn_container: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+    },
+    btn: {
+    backgroundColor: "#0782F9",
+    width: "70%",
+    padding: 15,
+    borderRadius: 10,
+    left: "15%",
+    },
+    btntext: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+    left: "15%"
+    },
+    buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
+    },
+    buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight: "700",
+    fontSize: 16,
+    },
+});
