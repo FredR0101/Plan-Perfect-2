2d8478bf6360ff7287fa064263fb747c51f34e68
Pirkhs <nathan.briers32@gmail.com>
2024-04-02 09:06:16 +0100

    further improvments to recommendations page

--- Components/EventCard.js
+++ Components/EventCard.js
@@ -1,11 +1,15 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export const EventCard = ({ event }) => {
+  const navigation = useNavigation()
   return (
+    <>
     <View style={styles.eventCard}>
+    <Pressable onPress={() => {navigation.navigate("Single Event", {event})}}> 
       <Image
         style={{
-          height: "200%",
+          height: "120%",
           width: "100%",
           border: "4px solid #C3ACD0",
           borderRadius: "5%",
@@ -17,15 +21,18 @@ export const EventCard = ({ event }) => {
       <Text style={styles.eventInfo}>{event.name}</Text>
       <Text style={styles.eventInfo}>{event.description}</Text>
       <Text style={styles.eventInfo}>Location: {event.location}</Text>
+
+      <Text style={styles.button}> Click On Event Card To View More </Text>
+    </Pressable>
     </View>
+    </>
   );
 };
 
 export const styles = StyleSheet.create({
   eventCard: {
     flex: 1,
-    marginTop: "40%",
-    marginBottom: "40%",
+    marginBottom: "85%",
     paddingTop: '30%',
     height: "100%",
     width: "100%",
@@ -33,7 +40,16 @@ export const styles = StyleSheet.create({
   },
   eventInfo: {
     textAlign: "left",
-    color: 'white',
-    
   },
+  button: {
+    backgroundColor: "#7743DB",
+    border: "4px solid #C3ACD0 ",
+    borderRadius: "10%",
+    color: "white",
+    width: "100%",
+    textAlign: "center",
+    float: "center",
+    padding: "5%",
+    fontSize: "80%"
+}
 });
--- Components/SingleEvent.js
+++ Components/SingleEvent.js
@@ -1,7 +1,121 @@
-import { Text } from "react-native"
+import { Text, View, Image, StyleSheet, Pressable, ScrollView } from "react-native"
+import { useState, useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { RadioButton } from 'react-native-paper';
+import { db } from "../firebase";
+import { updateDoc, collection, doc, getDocs, arrayUnion} from "firebase/firestore";
 
-export const SingleEvent = () => {
+
+export const SingleEvent = ({route: {params: {event}}}) => {
+    const navigation = useNavigation()
+    const [itineraryId, setItineraryId] = useState("first");
+    const [itineraries, setItineraries] = useState([])
+
+    const handleAddToItinerary = () => {
+        const eventToAdd = {...event}
+        delete eventToAdd.id
+
+        const activityRef = doc(db, 'test-activities', itineraryId);
+        updateDoc(activityRef, {
+            activities: arrayUnion(eventToAdd)
+        })
+        .then(() => { alert("Activity added to itinerary")})
+        .catch(() => { alert("Error when adding activity")})
+    }
+
+    useEffect(() => {
+        const fetchData = collection(db, "test-itineraries");
+        getDocs(fetchData).then((data) => {
+          const itineraryData = [];
+          data.docs.forEach((doc) => {
+            itineraryData.push({ ...doc.data(), id: doc.id });
+          });
+          setItineraries(itineraryData);
+        });
+      }, []);
+
+
     return (
-        <Text> Single Event </Text>
+        <>
+        <ScrollView style = {{marginLeft: "5%", marginRight: "5%", marginTop: "5%"}}>
+            <View >
+            <Pressable onPress = {() => navigation.navigate("Recommendations")}>
+                <Text style={styles.button}> Go Back to All Events </Text>
+            </Pressable>
+            </View>
+            <View style={styles.imageBox}>
+                <Image
+                style= {styles.image}
+                source={{
+                uri: event.image,
+                }}
+                />
+            </View>
+            <Text
+                style={{ textAlign: "left", fontWeight: "bold", paddingBottom: 5, fontSize: "110%" }}
+            >
+                {event.name}
+            </Text>
+            <Text style={styles.eventInfo}>{event.description}</Text>
+            <Text style={styles.eventInfo}>Location: {event.location}</Text>
+            <Text style={styles.eventInfo}>Price: £{event.price}</Text>
+            <Text style={styles.eventInfo}>Date: {event.date}</Text>
+            <Text style={styles.eventInfo}>
+                Number of people: {event.people}
+            </Text>
+           
+                <Text style={{ textAlign: "center", fontWeight: "bold", marginTop: "20%", fontSize: "110%" }}> Add to An Itinerary </Text>
+                {
+                itineraries.map(itinerary => {
+                    return (
+                        <View style={{flexDirection: "row"}} key={itinerary.id}>
+                            <RadioButton 
+                            value= {itinerary.id}
+                            status={ itineraryId === itinerary.id ? 'checked' : 'unchecked' }
+                            onPress={() => setItineraryId(itinerary.id)}
+                            />
+                            <Text style={{paddingTop: "3%"}}> {itinerary.name} </Text>
+                        </View>
+
+                    )
+                })
+                }
+
+                <Pressable onPress = {() => handleAddToItinerary()}>
+                    <Text style={styles.button}> Submit </Text>
+                </Pressable>
+        </ScrollView>
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+    imageBox: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    image: {
+        height: "200px",
+        width: "200px",
+        border: "4px solid #C3ACD0 ",
+        borderRadius: "100%",
+        },
+
+    eventInfo: {
+        textAlign: "left",
+    },
+    
+    button: {
+        marginBottom: "3%",
+        backgroundColor: "#7743DB",
+        border: "2px solid #C3ACD0 ",
+        borderRadius: "5%",
+        color: "white",
+        width: "50%",
+        textAlign: "center",
+        padding: "2%",
+    }
+})
+
+
