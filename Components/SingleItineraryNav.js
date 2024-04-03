dc8577a367888db0be2dbe602bc3ef68a25c9d1f
Ben McCarrick <b.mccarrick4326@gmail.com>
2024-04-03 16:23:57 +0100

    deleted share itinerary

--- Components/SingleItineraryNav.js
+++ Components/SingleItineraryNav.js
@@ -1,11 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Itinerary } from './Itinerary'
 import { AddActivity } from './AddActivity';
-import { ShareItinerary } from './ShareItinerary'
-import { useEffect, useState } from "react";
-import { db } from "../firebase";
-import { doc, getDocs, collection } from "firebase/firestore";
-
 
 export const SingleItineraryNav = ({route}) => {
     const Tab = createBottomTabNavigator();
@@ -17,7 +12,6 @@ export const SingleItineraryNav = ({route}) => {
         <Tab.Navigator screenOptions={{headerShown: false}} initialRouteName='Itinerary'>
             <Tab.Screen name="My Itinerary" component={Itinerary}/>
             <Tab.Screen name="Add Activity">{() => <AddActivity tripId={tripId}/>}</Tab.Screen>
-            <Tab.Screen name="Share Itinerary" component={ShareItinerary}/>
         </Tab.Navigator>
         </>
     )
