9b50040fcdfe08cd5327a60d571cfae5a04b2933
sclotaire <s.clotaire@hotmail.co.uk>
2024-04-03 10:37:09 +0100

    almost completed itinerary list

--- Components/ItinerariesList.js
+++ Components/ItinerariesList.js
@@ -5,12 +5,10 @@ import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SingleItineraryNav } from "./SingleItineraryNav";
 import { auth } from "../firebase";
-
-
+import Ionicons from '@expo/vector-icons/Ionicons';
 export const ItinerariesList = () => {
   const navigation = useNavigation();
   const [itinerary, setItinerary] = useState([]);
-
   useEffect(() => {
     const uid = auth.currentUser.uid;
     const fetchData = collection(db, "test-itineraries");
@@ -23,7 +21,6 @@ export const ItinerariesList = () => {
       setItinerary(itineraryData);
     });
   }, []);
-
   const handleOnPress = (deleteId) => {
    deleteDoc(doc(db, 'test-itineraries', deleteId))
    .then(() => {
@@ -35,14 +32,18 @@ export const ItinerariesList = () => {
     alert(err, 'Something went wrong')
    });
   };
-
   return (
     <>
-      <ScrollView>
+      <ScrollView style={styles.background}>
         {itinerary.map((data) => {
           return (
             <View style={styles.card} key={data.id}>
-              <Pressable
+                <Text style={styles.cardTitle}>Name: {data.name} </Text>
+                <Text style={styles.cardLocation}>Location: {data.location}</Text>
+                <Text style={styles.cardDates}>
+                  Dates: {data.startDate} to {data.endDate}
+                </Text>
+              <Pressable style={styles.viewTripButton}
                 onPress={() => {
                   navigation.navigate("Single Itinerary", {
                     screen: "My Itinerary",
@@ -54,16 +55,11 @@ export const ItinerariesList = () => {
                   <SingleItineraryNav tripId = {data.id}/>
                 }}
               >
-                <Text>Name: {data.name} </Text>
-                <Text>Location: {data.location}</Text>
-                <Text>
-                  Dates: {data.startDate} to {data.endDate}
-                </Text>
                 <Text>View Trip</Text>
               </Pressable>
-              <Pressable onPress={() => handleOnPress(data.id)}>
+              <Pressable style={styles.deleteButton} onPress={() => handleOnPress(data.id)}>
                 <Text>
-                  Delete Trip
+                  Delete
                 </Text>
               </Pressable>
             </View>
@@ -73,18 +69,79 @@ export const ItinerariesList = () => {
     </>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 50,
+  },
+  background: {
+    backgroundImage: 'linear-gradient(to bottom right, transparent, #7743DB)',
   },
   card: {
-    marginBottom: 50,
-    borderRadius: 12,
+    color: '#7743DB',
+    marginBottom: 20,
+    borderRadius: 30,
     borderWidth: 2,
-    borderColor: "#7fffd4",
+    backgroundColor: '#FFFBF5',
+    borderColor: "#FFFBF5",
     padding: 20,
+    marginHorizontal: 25,
+    marginTop: 20,
+  },
+  cardTitle: {
+    fontSize: 21,
+    fontWeight: 'bold',
+    color: '#7743DB',
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    marginBottom: 2,
+    shadowColor: '#7743DB',
+    textShadowColor: '#7743DB'
+  },
+  cardLocation: {
+    fontSize: 16,
+    fontWeight: '450',
+    color: '#7743DB',
+    textAlign: 'center'
+  },
+  cardDates: {
+    color: '#7743DB',
+    justifyContent: 'center',
+    margin: 2,
+    marginBottom: 7,
+    textAlign: 'center'
+  },
+  deleteButton: {
+    color: '#7743DB',
+    borderRadius: 5,
+    borderWidth: -3,
+    backgroundColor: '#F7EFE5',
+    borderColor: "#F7EFE5",
+    padding: 10,
+    marginRight: 250,
+    alignItems: 'center',
+    alignContent: 'center',
+    width: 38,
+    position: 'absolute',
+    bottom: 2,
+    right: 20,
+    height: 36,
+    elevation: 5
+  },
+  viewTripButton: {
+    width: "80%",
+    backgroundColor: "#7743DB",
+    height: 40,
+    paddingTop: 10,
+    marginBottom: 10,
+    color: "white",
+    borderRadius: 2,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    border: "1px solid #7743DB",
+    borderRadius: 10,
+    bottom: -10,
+    left: 33
   },
 });
